560a8c26fee232c7568dd662319b03a7ec718322
Jonas Poulsen <schoop2002@live.dk>
2020-04-14 08:42:46 +0200

    Progress på adgangsviseren

--- Eksamensprojekt Adgangskortprogram/Eksamensprojekt Adgangskortprogram/AdgangskortSide.Designer.vb
+++ Eksamensprojekt Adgangskortprogram/Eksamensprojekt Adgangskortprogram/AdgangskortSide.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class AdgangskortSide
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,164 @@ Partial Class AdgangskortSide
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.AdgangsSide_Overskrift = New System.Windows.Forms.Label()
+        Me.AdgangsSide_Filler = New System.Windows.Forms.Label()
+        Me.DD_Biologi = New System.Windows.Forms.ComboBox()
+        Me.DD_Dansk = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox7 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox8 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox9 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox10 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'AdgangsSide_Overskrift
+        '
+        Me.AdgangsSide_Overskrift.AutoSize = True
+        Me.AdgangsSide_Overskrift.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AdgangsSide_Overskrift.Location = New System.Drawing.Point(13, 13)
+        Me.AdgangsSide_Overskrift.Name = "AdgangsSide_Overskrift"
+        Me.AdgangsSide_Overskrift.Size = New System.Drawing.Size(186, 29)
+        Me.AdgangsSide_Overskrift.TabIndex = 0
+        Me.AdgangsSide_Overskrift.Text = "Adgangsviseren"
+        '
+        'AdgangsSide_Filler
+        '
+        Me.AdgangsSide_Filler.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.AdgangsSide_Filler.Location = New System.Drawing.Point(18, 51)
+        Me.AdgangsSide_Filler.Name = "AdgangsSide_Filler"
+        Me.AdgangsSide_Filler.Size = New System.Drawing.Size(181, 366)
+        Me.AdgangsSide_Filler.TabIndex = 1
+        '
+        'DD_Biologi
+        '
+        Me.DD_Biologi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.DD_Biologi.FormattingEnabled = True
+        Me.DD_Biologi.Location = New System.Drawing.Point(27, 60)
+        Me.DD_Biologi.Name = "DD_Biologi"
+        Me.DD_Biologi.Size = New System.Drawing.Size(157, 21)
+        Me.DD_Biologi.TabIndex = 2
+        '
+        'DD_Dansk
+        '
+        Me.DD_Dansk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.DD_Dansk.FormattingEnabled = True
+        Me.DD_Dansk.Location = New System.Drawing.Point(27, 96)
+        Me.DD_Dansk.Name = "DD_Dansk"
+        Me.DD_Dansk.Size = New System.Drawing.Size(157, 21)
+        Me.DD_Dansk.TabIndex = 3
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(27, 132)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox3.TabIndex = 4
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(27, 168)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox4.TabIndex = 5
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(27, 204)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox5.TabIndex = 6
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Location = New System.Drawing.Point(27, 240)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox6.TabIndex = 7
+        '
+        'ComboBox7
+        '
+        Me.ComboBox7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox7.FormattingEnabled = True
+        Me.ComboBox7.Location = New System.Drawing.Point(27, 276)
+        Me.ComboBox7.Name = "ComboBox7"
+        Me.ComboBox7.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox7.TabIndex = 8
+        '
+        'ComboBox8
+        '
+        Me.ComboBox8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox8.FormattingEnabled = True
+        Me.ComboBox8.Location = New System.Drawing.Point(27, 312)
+        Me.ComboBox8.Name = "ComboBox8"
+        Me.ComboBox8.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox8.TabIndex = 9
+        '
+        'ComboBox9
+        '
+        Me.ComboBox9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox9.FormattingEnabled = True
+        Me.ComboBox9.Location = New System.Drawing.Point(27, 348)
+        Me.ComboBox9.Name = "ComboBox9"
+        Me.ComboBox9.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox9.TabIndex = 10
+        '
+        'ComboBox10
+        '
+        Me.ComboBox10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox10.FormattingEnabled = True
+        Me.ComboBox10.Location = New System.Drawing.Point(27, 384)
+        Me.ComboBox10.Name = "ComboBox10"
+        Me.ComboBox10.Size = New System.Drawing.Size(157, 21)
+        Me.ComboBox10.TabIndex = 11
+        '
+        'AdgangskortSide
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox10)
+        Me.Controls.Add(Me.ComboBox9)
+        Me.Controls.Add(Me.ComboBox8)
+        Me.Controls.Add(Me.ComboBox7)
+        Me.Controls.Add(Me.ComboBox6)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.DD_Dansk)
+        Me.Controls.Add(Me.DD_Biologi)
+        Me.Controls.Add(Me.AdgangsSide_Filler)
+        Me.Controls.Add(Me.AdgangsSide_Overskrift)
+        Me.Name = "AdgangskortSide"
         Me.Text = "AdgangskortSide"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents AdgangsSide_Overskrift As Label
+    Friend WithEvents AdgangsSide_Filler As Label
+    Friend WithEvents DD_Biologi As ComboBox
+    Friend WithEvents DD_Dansk As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents ComboBox7 As ComboBox
+    Friend WithEvents ComboBox8 As ComboBox
+    Friend WithEvents ComboBox9 As ComboBox
+    Friend WithEvents ComboBox10 As ComboBox
 End Class
